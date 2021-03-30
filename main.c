f52c9a645aed0e8f601542c0e656a86982f36d25
Pranavaraaj <81604865+Pranavaraaj@users.noreply.github.com>
2021-03-30 10:00:58 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{   int n,val;
+    char name[20],sex[6], parent1[20],parent2[20];
+    scanf("%d",&n);
+    for(int i=0; i<=n;i++)
+    {
+        scanf("%s",&name);
+        scanf("%s",&sex)
+        scanf("%s",&parent1);
+        scanf("%s",&parent2);
+    }
+    
+    for(int j=0;j<=n;j++)
+     for(int i=0;i<=4;i++)
+    {
+        val= strcmp();
+    }
+    
+}
